955a97a4581d30dba2f15338a97770b1ae4599f2
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-16 00:16:20 +0300

    Update LibraryUnit.h

--- Repositories/LibraryUnit.h
+++ Repositories/LibraryUnit.h
@@ -1,48 +1,47 @@
 #pragma once
 #include <string>
 #include <vector>
-//<cstdlib>
-
-using StringArray = std::vector<std::string>;  // move this
 
 class LibraryUnit
 {
 public:
 
-	const std::string getTitle() const;
-	const std::string getPublisher() const;
-	const std::string getGenre() const;
-	const std::string getBriefDescription() const;
+	LibraryUnit() = delete;
+	LibraryUnit(std::string title, std::string publisher, std::string genre, std::string description,
+		unsigned short releaseYear, unsigned short rating);
+	virtual ~LibraryUnit() = default;
+
+	LibraryUnit(const LibraryUnit& other);
+	LibraryUnit& operator=(const LibraryUnit& other);
+
+	virtual LibraryUnit* clone() const = 0;
+
+	const std::string& getTitle() const;
+	const std::string& getPublisher() const;
+	const std::string& getGenre() const;
+	const std::string& getBriefDescription() const;
 	const unsigned short getReleaseYear() const;
-	const unsigned getUniceNumber() const;
-	const double getRating() const;
+	const unsigned short getRating() const;
+	const unsigned getUniqueNumber() const;
 
 	void setTitle(const std::string& str);
 	void setPublisher(const std::string& str);
 	void setGenre(const std::string& str);
 	void setBriefDescription(const std::string& str);
 	void setReleaseYear(unsigned short year);
-	void setRating(double rating);
-
-	unsigned generateRandomNumber();
-
-protected: //  ?? so there cant be LibraryUnit object
-
-	LibraryUnit();
-	LibraryUnit(std::string title, std::string publisher, std::string genre, std::string description,
-		short releaseYear, double rating);
-	~LibraryUnit();
-
-	LibraryUnit(const LibraryUnit& other);
-	LibraryUnit& operator=(const LibraryUnit& other);
+	void setRating(unsigned short rating);
 
 protected:
 
 	std::string title;
 	std::string publisher;
 	std::string genre; // Did't make it enum, beacuse 26 genres seems a bit too much
-	std::string briefDescription;
+	std::string description;
 	unsigned short releaseYear;
-	const unsigned uniceNumber; // ?? should it be const
-	double rating;
+	unsigned short rating;
+	unsigned uniqueNumber;
+
+private:
+
+	void copyFrom(const LibraryUnit& other);
 };
