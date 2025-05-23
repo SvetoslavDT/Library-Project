58828728376ab9aed63219f0faab36c3ef900636
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-24 02:05:27 +0300

    Update Series.h

--- Repositories/Series.h
+++ Repositories/Series.h
@@ -1,7 +1,20 @@
 #pragma once
-// what shall I include
+#include "Book.h"
+#include "Periodical.h"
 
-class Series // : __________
+class Series : public Book, public Periodical
 {
+public:
+
+	Series();
+	Series(const std::string& title, const std::string& publisher, const std::string& genre,
+		const std::string& description, unsigned short releaseYear, unsigned short rating,
+		const std::string& author, const std::string& isbn = "", unsigned short month,
+		const std::vector<Article>& articles, const std::string& issn = "");
+	Series(const Series& other);
+	Series& operator=(const Series& other);
+	~Series() = default;
+
+	LibraryUnit* clone() const override;
 
 };
