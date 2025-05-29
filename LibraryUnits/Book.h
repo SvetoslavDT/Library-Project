c2a194143e07a4e4480a43b47a00e5752fc4e02c
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-29 22:50:57 +0300

    Create Book.h

---
+++ LibraryUnits/Book.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "LibraryUnit.h"
+#include "Optional.h"
+#include "Functions.h"
+
+class Book : public virtual LibraryUnit
+{
+public:
+
+	Book() = default;
+	Book(const std::string& title, const std::string& publisher, const std::string& genre, const std::string& description,
+		unsigned short releaseYear, unsigned short rating, const std::string& author, const std::string& isbn = "");
+	Book(const Book& other);
+	Book& operator=(const Book& other);
+
+	const std::string getISBN() const; // ?? Pokaji na Tedo
+	const std::string& getAuthor() const;
+	const StringArray& getKeyWords() const;
+
+	LibraryUnit* clone() const override;
+
+	//Should the setters be private ( I feel like I shouldnt mendle in how a book will be handled )
+	//I just have to give the possibility if needed
+	void pushKeyWords();
+	void setISBN(const std::string& str = generateNString(Book::ISBN_LENGTH));
+	void setAuthor(const std::string& name);
+
+	friend std::ostream& operator<<(std::ostream& os, const Book& obj);
+	friend std::istream& operator>>(std::istream& is, Book& obj);
+
+	void writeToBinary(std::ostream& os) const override;
+	void readFromBinary(std::istream& is) override;
+
+protected:
+
+	Optional<std::string> ISBN;
+	std::string author;
+	StringArray keyWords;
+	
+	const static unsigned short ISBN_LENGTH;
+};
