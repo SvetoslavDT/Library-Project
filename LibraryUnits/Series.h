df674cb74869514b590a411cb644cfa1c5c4ec17
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-29 22:52:10 +0300

    Create Series.h

---
+++ LibraryUnits/Series.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Book.h"
+#include "Periodical.h"
+
+class Series : public Book, public Periodical
+{
+public:
+
+	Series();
+	Series(const std::string& title, const std::string& publisher, const std::string& genre,
+		const std::string& description, unsigned short releaseYear, unsigned short rating,
+		const std::string& author, const std::string& isbn = "", unsigned short month,
+		const std::vector<Article>& articles, const std::string& issn = "");
+	Series(const Series& other);
+	Series& operator=(const Series& other);
+
+	LibraryUnit* clone() const override;
+
+	friend std::ostream& operator<<(std::ostream& os, const Series& obj);
+	friend std::istream& operator>>(std::istream& is, const Series& obj);
+
+	void writeToBinary(std::ostream& os) const override;
+	void readFromBinary(std::istream& is) override;
+
+};
