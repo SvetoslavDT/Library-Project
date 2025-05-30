1eda1a2bd94bc154c73bb67e9312a03167c041a0
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-30 22:31:40 +0300

    Update Series.cpp

--- LibraryUnits/Series.cpp
+++ LibraryUnits/Series.cpp
@@ -1,27 +1,12 @@
 #include "Series.h"
 
-Series::Series() : LibraryUnit(), Book(), Periodical() {}
-
 Series::Series(const std::string& title, const std::string& publisher, const std::string& genre,
 	const std::string& description, unsigned short releaseYear, unsigned short rating,
-	const std::string& author, const std::string& isbn, unsigned short month,
-	const std::vector<Article>& articles, const std::string& issn)
+	const std::string& author, unsigned short month, const std::vector<Article>& articles,
+	const std::string& isbn = "", const std::string& issn = "")
 	: LibraryUnit(title, publisher, genre, description, releaseYear, rating),
-	  Book(title, publisher, genre, description, releaseYear, rating, author),
+	  Book(title, publisher, genre, description, releaseYear, rating, author, isbn),
 	  Periodical(title, publisher, genre, description, releaseYear, rating, month, articles, issn) {}
-
-Series::Series(const Series& other) : LibraryUnit(other), Book(other), Periodical(other) {}
-
-Series& Series::operator=(const Series& other)
-{
-	if (this != &other)
-	{
-		LibraryUnit::operator=(other);
-		Book::operator=(other);
-		Periodical::operator=(other);
-	}
-	return *this;
-}
 
 LibraryUnit* Series::clone() const
 {
@@ -44,10 +29,14 @@ std::ostream& operator<<(std::ostream& os, const Series& obj)
 {
 	os << (const Book&)obj;
 	os << (const Periodical&)obj;
+
+	return os;
 }
 
 std::istream& operator>>(std::istream& is, const Series& obj)
 {
 	is >> (Book&)obj;
 	is >> (Periodical&)obj;
+
+	return is;
 }
