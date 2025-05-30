702f7c0478cfa03ea034545c2d067186656a09d5
SvetoslavDT <svetcvetkov@gmail.com>
2025-05-30 22:31:27 +0300

    Update Series.h

--- LibraryUnits/Series.h
+++ LibraryUnits/Series.h
@@ -6,13 +6,11 @@ class Series : public Book, public Periodical
 {
 public:
 
-	Series();
+	Series() = default;
 	Series(const std::string& title, const std::string& publisher, const std::string& genre,
 		const std::string& description, unsigned short releaseYear, unsigned short rating,
-		const std::string& author, const std::string& isbn = "", unsigned short month,
-		const std::vector<Article>& articles, const std::string& issn = "");
-	Series(const Series& other);
-	Series& operator=(const Series& other);
+		const std::string& author, unsigned short month, const std::vector<Article>& articles,
+		const std::string& isbn, const std::string& issn);
 
 	LibraryUnit* clone() const override;
 
