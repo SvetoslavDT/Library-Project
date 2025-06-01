9276a1fd7fc48760d31bdc01f5ed54745bbcaa60
SvetoslavDT <svetcvetkov@gmail.com>
2025-06-01 19:26:58 +0300

    Update Series.h

--- LibraryUnits/Series.h
+++ LibraryUnits/Series.h
@@ -10,14 +10,15 @@ public:
 	Series(const std::string& title, const std::string& publisher, const std::string& genre,
 		const std::string& description, unsigned short releaseYear, unsigned short rating,
 		const std::string& author, unsigned short month, const std::vector<Article>& articles,
-		const std::string& isbn, const std::string& issn);
+		const std::string& isbn = "", const std::string& issn = "");
 
 	LibraryUnit* clone() const override;
 
-	friend std::ostream& operator<<(std::ostream& os, const Series& obj);
+	//operator>> polymorf. called from LibraryUnit
 	friend std::istream& operator>>(std::istream& is, const Series& obj);
 
 	void writeToBinary(std::ostream& os) const override;
 	void readFromBinary(std::istream& is) override;
 
+	void print(std::ostream& os) const override;
 };
