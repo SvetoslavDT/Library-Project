44ba482f3104cc61a271071fc3d5907311c8eeef
SvetoslavDT <svetcvetkov@gmail.com>
2025-06-18 20:08:58 +0300

    Update Reader.cpp

--- UserClasses/Reader.cpp
+++ UserClasses/Reader.cpp
@@ -1,16 +1,17 @@
 #include "Reader.h"
+#include "Functions.h"
 
 Reader::LibraryUnitTaken::LibraryUnitTaken(const LibraryUnit* unit, const Date& borrowDate, const Date& returnDate)
-	: borrowDate(borrowDate), returnDate(returnDate)
+	: unit(nullptr), borrowDate(borrowDate), returnDate(returnDate)
 {
 	if (returnDate < borrowDate)
 		throw std::logic_error("Can not return a unit before borrowing it");
 
-	unit = unit->clone();
+	this->unit = unit->clone();
 }
 
 Reader::LibraryUnitTaken::LibraryUnitTaken(const LibraryUnitTaken& other)
-	: borrowDate(borrowDate), returnDate(returnDate)
+	: borrowDate(other.borrowDate), returnDate(other.returnDate)
 {
 	unit = other.unit->clone();
 }
@@ -19,9 +20,10 @@ Reader::LibraryUnitTaken& Reader::LibraryUnitTaken::operator=(const LibraryUnitT
 {
 	if (this != &other)
 	{
-		unit = other.unit->clone();
 		borrowDate = other.borrowDate;
 		returnDate = other.returnDate;
+		delete unit;
+		unit = other.unit->clone();
 	}
 	return *this;
 }
@@ -55,16 +57,50 @@ void Reader::LibraryUnitTaken::seReturnDate(const Date& date)
 
 void Reader::LibraryUnitTaken::writeToBinary(std::ostream& os) const
 {
+	std::string type = unit->getType();
+	FunctionsForBinary::writeString(os, type);
+
 	unit->writeToBinary(os);
+
 	borrowDate.writeDateToBinary(os);
 	returnDate.writeDateToBinary(os);
 }
 
 void Reader::LibraryUnitTaken::readFromBinary(std::istream& is)
 {
-	unit->readFromBinary(is);
-	borrowDate.readDateFromBinary(is);
-	borrowDate.readDateFromBinary(is);
+	size_t len;
+	is.read(reinterpret_cast<char*>(&len), sizeof(len));
+	std::string type(len, '\0');
+	is.read(&type[0], len);
+
+	LibraryUnit* tmpUnit = nullptr;
+	if (type == "Book")
+		tmpUnit = new Book();
+	else if (type == "Periodical")
+		tmpUnit = new Periodical();
+	else if (type == "Series")
+		tmpUnit = new Series();
+	else
+		throw std::runtime_error("Unknown LibraryUnit type.");
+
+	tmpUnit->readFromBinary(is);
+
+	Date tmpBorrow, tmpReturn;
+	tmpBorrow.readDateFromBinary(is);
+	tmpReturn.readDateFromBinary(is);
+
+	delete unit;
+	unit = tmpUnit;
+	borrowDate = tmpBorrow;
+	returnDate = tmpReturn;
+}
+
+void Reader::LibraryUnitTaken::print() const
+{
+	unit->print();
+	
+	std::cout << "Borrow date - " << borrowDate << '\n';
+	std::cout << "Return date - " << returnDate << '\n';
 }
 
 Reader::Reader(const std::string& name, const std::string& password, const std::vector<LibraryUnitTaken>& arr)
@@ -73,7 +109,12 @@ Reader::Reader(const std::string& name, const std::string& password, const std::
 Reader::Reader(const std::string& name, const std::string& password)
 	: User(name, password) {}
 
-const std::vector<Reader::LibraryUnitTaken> Reader::getTakenUnits() const
+const std::vector<Reader::LibraryUnitTaken>& Reader::getTakenUnits() const
+{
+	return takenUnits;
+}
+
+std::vector<Reader::LibraryUnitTaken>& Reader::getTakenUnits()
 {
 	return takenUnits;
 }
@@ -107,23 +148,131 @@ void Reader::readFromBinary(std::istream& is)
 {
 	User::readFromBinary(is);
 
+	size_t count;
+	is.read((char*)&count, sizeof(count));
+
+	std::vector<LibraryUnitTaken> tmp;
+	tmp.reserve(count);
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		LibraryUnitTaken lut;
+		lut.readFromBinary(is);
+		tmp.push_back(lut);
+	}
+
+	takenUnits.swap(tmp);
+}
+
+void Reader::serialise(std::ostream& os) const
+{
+	User::serialise(os);
+
+	os << takenUnits.size() << '\n';
+	for (size_t i = 0; i < takenUnits.size(); i++)
+		os << takenUnits[i] << '\n';
+}
+
+void Reader::deserialize(std::istream& is)
+{
+	User::deserialize(is);
+
 	size_t size;
-	is.read((char*)&size, sizeof(size));
-	takenUnits.clear();
-	takenUnits.resize(size);
+	if(!(is >> size)) throw std::exception("Stream failed");
+	std::vector<LibraryUnitTaken> unitsTaken(size);
 	for (size_t i = 0; i < size; i++)
-		takenUnits[i].readFromBinary(is);
+	{
+		if(!(is >> unitsTaken[i])) throw std::exception("Stream failed");
+	}
+
+	setTakenLibraryUnits(unitsTaken);
 }
 
 std::ostream& operator<<(std::ostream& os, const Reader::LibraryUnitTaken& obj)
 {
-	return os << *obj.unit << '\n' << obj.borrowDate << '\n' << obj.returnDate << '\n';
+	return os << obj.unit->getType() << *obj.unit << '\n' << obj.borrowDate
+		<< '\n' << obj.returnDate << '\n';
 }
 
-void Reader::print(std::ostream& os) const
+std::istream& operator>>(std::istream& is, Reader::LibraryUnitTaken& obj)
 {
-	User::print(os);
+	std::string type;
+	if (!std::getline(is, type))
+		throw std::runtime_error("Stream failed reading type");
+
+	LibraryUnit* pointer = nullptr;
+
+	if (type == "Book")
+	{
+		Book* newBook = new Book();
+		if (!(is >> *newBook))
+		{
+			delete newBook;
+			throw std::runtime_error("Stream failed reading Book");
+		}
+		pointer = newBook;
+	}
+	else if (type == "Periodical")
+	{
+		Periodical* newPer = new Periodical();
+		if (!(is >> *newPer))
+		{
+			delete newPer;
+			throw std::runtime_error("Stream failed reading Periodical");
+		}
+		pointer = newPer;
+	}
+	else if (type == "Series")
+	{
+		Series* series = new Series();
+		if (!(is >> *series))
+		{
+			delete series;
+			throw std::runtime_error("Stream failed reading Series");
+		}
+		pointer = series;
+	}
+	else
+	{
+		throw std::invalid_argument("Unknown LibraryUnit type: " + type);
+	}
+
+	Date borDate, retDate;
+	if (!(is >> borDate >> retDate))
+	{
+		delete pointer;
+		throw std::runtime_error("Stream failed reading dates");
+	}
+
+	if (borDate > retDate)
+	{
+		delete pointer;
+		throw std::logic_error("Cannot return a book before taking it.");
+	}
+
+	delete obj.unit;
+	obj.unit = pointer;
+	obj.borrowDate = borDate;
+	obj.returnDate = retDate;
+	return is;
+}
+
+void Reader::print() const
+{
+	User::print();
+
+	std::cout << '\n';
 
 	for (size_t i = 0; i < takenUnits.size(); i++)
-		os << takenUnits[i];
+		takenUnits[i].print();
+}
+
+std::string Reader::getType() const
+{
+	return "Reader";
+}
+
+unsigned Reader::getPrintLines() const
+{
+	return User::getPrintLines() + 12;
 }
