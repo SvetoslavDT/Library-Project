42b81fafa58aeb7503b97ffd417125e0a4902016
SvetoslavDT <svetcvetkov@gmail.com>
2025-06-18 20:01:52 +0300

    Create LibraryUnitRepository.h

---
+++ Repositories/LibraryUnitRepository.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "LibraryUnit.h"
+#include <iostream>
+#include <fstream>
+
+class LibraryUnitRepository
+{
+public:
+
+	LibraryUnitRepository();
+	~LibraryUnitRepository();
+
+	std::vector<LibraryUnit*>& getUnits();
+	const std::vector<LibraryUnit*>& getUnits() const;
+
+	size_t readUntilFindByUniqueNumber(unsigned num);
+
+	void readAllBooks();
+	void readAllPeriodicals();
+	void readAllSeries();
+
+	void readToEndAllFiles();
+	void writeAllToFiles();
+
+private:
+
+	void readAllFromFile(std::fstream& stream, const char* fileName);
+
+	void openStream(std::fstream& stream, const char* fileName);
+	void closeAllStreams();
+
+	const char booksFile[20]	   = "books.dat";
+	const char periodicalsFile[20] = "periodical.dat";
+	const char seriesFile[20]	   = "series.dat";
+
+	std::vector<LibraryUnit*> units;
+
+	std::fstream booksStream;
+	std::fstream periodicalsStream;
+	std::fstream seriesStream;
+};
