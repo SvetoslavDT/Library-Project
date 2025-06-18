34b5fa3acbb7cb8dec4506dd409b12d409089d1d
SvetoslavDT <svetcvetkov@gmail.com>
2025-06-18 20:09:17 +0300

    Update Series.cpp

--- LibraryUnits/Series.cpp
+++ LibraryUnits/Series.cpp
@@ -15,19 +15,55 @@ LibraryUnit* Series::clone() const
 
 void Series::writeToBinary(std::ostream& os) const
 {
-	Book::writeToBinary(os);
-	Periodical::writeToBinary(os);
+	LibraryUnit::writeToBinary(os);
+
+	ISBN.writeToBinary(os);
+	FunctionsForBinary::writeString(os, author);
+	FunctionsForBinary::writeStringArray(os, keyWords);
+
+	os.write(reinterpret_cast<const char*>(&month), sizeof(month));
+	ISSN.writeToBinary(os);
+
+	size_t count = articles.size();
+	os.write(reinterpret_cast<const char*>(&count), sizeof(count));
+	for (auto& art : articles)
+		art.writeToBinary(os);
 }
 
 void Series::readFromBinary(std::istream& is)
 {
-	Book::readFromBinary(is);
-	Periodical::readFromBinary(is);
+	Series tmp; 
+
+	tmp.LibraryUnit::readFromBinary(is);
+
+	tmp.ISBN.readFromFile(is);
+	tmp.author = FunctionsForBinary::readString(is);
+	tmp.keyWords = FunctionsForBinary::readStringArray(is);
+
+	unsigned short m;
+	is.read((char*)&m, sizeof(m));
+	tmp.month = m;
+
+	tmp.ISSN.readFromFile(is);
+
+	size_t count;
+	is.read((char*)&count, sizeof(count));
+
+	tmp.articles.clear();
+	tmp.articles.reserve(count);
+	for (size_t i = 0; i < count; ++i)
+	{
+		Article art;
+		art.readFromBinary(is);
+		tmp.articles.push_back(std::move(art));
+	}
+
+	*this = std::move(tmp);
 }
 
-void Series::print(std::ostream& os) const
+void Series::serialise(std::ostream& os) const
 {
-	Book::print(os);
+	Book::serialise(os);
 
 	os << month << '\n' << ISSN << '\n';
 	os << articles.size() << '\n';
@@ -38,10 +74,64 @@ void Series::print(std::ostream& os) const
 	}
 }
 
-std::istream& operator>>(std::istream& is, const Series& obj)
+void Series::deserialize(std::istream& is)
 {
-	is >> (Book&)obj;
-	is >> (Periodical&)obj;
+	Book::deserialize(is);
 
-	return is;
+	unsigned short month;
+	Optional<std::string> issn;
+
+	if (!(is >> month)) throw std::exception("Stream failed");
+	if (!(is >> issn)) throw std::exception("Stream failed");
+
+	size_t articlesCount;
+	std::string temp;
+	is >> articlesCount;
+
+	std::vector<Article> newArticles(articlesCount);
+
+	for (size_t i = 0; i < articlesCount; ++i)
+	{
+		if (!(is >> newArticles[i])) throw std::exception("Stream failed");
+	}
+
+	setMonth(month);
+	setISSN(issn.getValue());
+	setArticles(newArticles);
+}
+
+void Series::print() const
+{
+	Book::print();
+
+	std::cout << "Month - " << month << '\n';
+	if (ISSN.hasValue())
+	{
+		for (size_t i = 0; i < ISSN.getValue().length(); i++)
+		{
+			if (i == 4)
+				std::cout << '-';
+			std::cout << ISSN.getValue()[i];
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+
+	for (size_t i = 0; i < articles.size(); i++)
+	{
+		std::cout << articles[i];
+		if (i != articles.size() - 1)
+			std::cout << '\n';
+	}
+}
+
+std::string Series::getType() const
+{
+	return "Series";
+}
+
+unsigned Series::getPrintLines() const
+{
+	return Periodical::getPrintLines() + 2;
 }
